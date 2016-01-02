45433aa6797eea827cf2a6078bfefdc934857704
Brandon Silver <brandon@brandonsilver.com>
2016-01-02 13:40:32 -0600

    add authorship/copyright/license headers

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -1,3 +1,7 @@
+// Author: Brandon Silver
+// Adapted from original Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
 // Dup1 prints the text of each line that appears more than once in the standard
 // input, preceded by its count.
 package main
--- ch1/echo3/main.go
+++ ch1/echo3/main.go
@@ -1,3 +1,7 @@
+// Author: Brandon Silver
+// Adapted from original Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
 // Echo3 prints its command line arguments.
 package main
 
