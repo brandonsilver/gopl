e0d0ca2ce37433e47d5572a5a4e8fac14c1a2f26
Brandon Silver <brandon@brandonsilver.com>
2016-01-02 15:26:41 -0600

    complete exercises 1.5 and 1.6

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -15,11 +15,15 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White, color.Black}
+// Exercise 1.5: Change the color palette to green on black
+// Exercise 1.6: Modify the program to produce images in multiple colors
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0x80, 0x00, 0xff}, color.RGBA{0xFF, 0x00, 0x00, 0xff}, color.RGBA{0x00, 0x00, 0xFF, 0xff}}
 
 const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	blackIndex = 0 // first color in palette
+	greenIndex = 1 // next color in palette
+	redIndex   = 2
+	blueIndex  = 3
 )
 
 func main() {
@@ -37,15 +41,22 @@ func lissajous(out io.Writer) {
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
+	var currentColorIndex uint8
+	currentColorIndex = greenIndex
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), currentColorIndex)
 		}
 		phase += 0.1
+		currentColorIndex = (currentColorIndex + 1) % 4 // move to the next color
+		// skip black (background color)
+		if currentColorIndex == 0 {
+			currentColorIndex++
+		}
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
